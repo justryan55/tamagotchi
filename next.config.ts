a6da0f1d5d522b8238e74470280f7d26dd0f7cc6
Ryan <ryan.irani@gmail.com>
2025-03-19 22:28:07 +0000

    add weather API key

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   devIndicators: false,
   transpilePackages: ["three"],
+  env: {
+    WEATHER_API_KEY: process.env.WEATHER_API_KEY,
+  },
 };
 
 export default nextConfig;
