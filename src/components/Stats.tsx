d75b9aebac76fbbc4cf09407eff68e24d2a3f09d
Ryan <ryan.irani@gmail.com>
2025-03-19 19:03:45 +0000

    add setState context

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -6,15 +6,16 @@ interface StatsProps {
 }
 
 export default function Stats({ toggleStats }: StatsProps) {
-  const stats = useStats();
+  const { stats, setStats } = useStats();
   return (
     <div className={`${styles.container} ${toggleStats ? "" : styles.hidden}`}>
-      {/* <h1 className={styles.heading}>Stats</h1> */}
       <ul>
         {Object.entries(stats).map((item) => (
           <li className={styles.list} key={item[1].id}>
             <h2 className={styles.subheading}>{item[1].title}</h2>
-            <span className={styles.bar}>
+            <span
+              className={item[1].title === "XP" ? styles["xp-bar"] : styles.bar}
+            >
               <span style={{ width: `${item[1].value}%` }}></span>
             </span>
           </li>
