f37dce68ce93f4e4c202f9c598e063853a91ad50
Ryan <ryan.irani@gmail.com>
2025-03-20 09:51:08 +0000

    add deplete hunger function

--- src/components/DepleteStats.tsx
+++ src/components/DepleteStats.tsx
@@ -1,23 +1,35 @@
 import { useStats } from "@/providers/StatsProvider";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function DepleteStats() {
   const { stats, setStats } = useStats();
+  const [time, setTime] = useState<number>(Date.now());
 
-  function generateRandomTime() {
-    return 5000;
-  }
+  const getIntervalTime = () => {
+    return Math.random() * 20000 + 10000;
+  };
 
   useEffect(() => {
-    const interval = setInterval(function () {
-      clearInterval(interval);
+    const interval = setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = (currentTime - time) / 1000;
+      setTime(currentTime);
 
       setStats((prevStats) => ({
         ...prevStats,
-        hunger: { ...prevStats.hunger, value: prevStats.hunger.value - 1 },
+        hunger: {
+          ...prevStats.hunger,
+          value: prevStats.hunger.value - 1,
+        },
       }));
-    }, generateRandomTime());
-  }, [stats, setStats]);
+
+      localStorage.setItem("stats", JSON.stringify(stats));
+    }, getIntervalTime());
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   return null;
 }
