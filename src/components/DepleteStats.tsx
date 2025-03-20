04bdbeed08dee399bb45c08648e5199467f7c78e
Ryan <ryan.irani@gmail.com>
2025-03-20 10:32:03 +0000

    update deplete hunger function

--- src/components/DepleteStats.tsx
+++ src/components/DepleteStats.tsx
@@ -15,13 +15,36 @@ export default function DepleteStats() {
       const deltaTime = (currentTime - time) / 1000;
       setTime(currentTime);
 
-      setStats((prevStats) => ({
-        ...prevStats,
-        hunger: {
-          ...prevStats.hunger,
-          value: prevStats.hunger.value - 1,
-        },
-      }));
+      setStats((prevStats) => {
+        const newHunger = Math.max(
+          0,
+          prevStats.hunger.value - (Math.random() * 2 + 1)
+        );
+        const newHappiness = Math.max(
+          0,
+          prevStats.happiness.value - (Math.random() * 1.5 + 1)
+        );
+
+        const newHygiene = Math.max(
+          0,
+          prevStats.hygiene.value - (Math.random() * 2 + 1)
+        );
+
+        const healthLoss = Math.abs(newHappiness - newHunger);
+        const newHealth = Math.max(0, prevStats.health.value - healthLoss);
+
+        const energyLoss = Math.abs(newHealth - newHunger) / 2;
+        const newEnergy = Math.max(0, prevStats.energy.value - energyLoss);
+
+        return {
+          ...prevStats,
+          hunger: { ...prevStats.hunger, value: newHunger },
+          happiness: { ...prevStats.happiness, value: newHappiness },
+          health: { ...prevStats.health, value: newHealth },
+          energy: { ...prevStats.energy, value: newEnergy },
+          hygiene: { ...prevStats.hygiene, value: newHygiene },
+        };
+      });
 
       localStorage.setItem("stats", JSON.stringify(stats));
     }, getIntervalTime());
