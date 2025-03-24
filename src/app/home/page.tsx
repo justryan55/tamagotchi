73c81d759d64db6a56193dd4b3596c3e862e4aa3
Ryan <ryan.irani@gmail.com>
2025-03-24 09:59:55 +0000

    add checkstats property and spawn poo prop

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import AnimationList from "@/components/AnimationList";
+import CheckStats from "@/components/CheckStats";
 import DepleteStats from "@/components/DepleteStats";
 import Experience from "@/components/Experience";
 import Nav from "@/components/Nav";
@@ -12,6 +13,7 @@ export default function Page() {
   const [toggleStats, setToggleStats] = useState(false);
   const [toggleAnimations, setToggleAnimations] = useState(false);
   const [animation, setAnimation] = useState("standing");
+  const [spawnPoo, setSpawnPoo] = useState(false);
 
   const toggleStatsClass = () => {
     setToggleStats((prev) => !prev);
@@ -29,11 +31,12 @@ export default function Page() {
 
   return (
     <>
+      <CheckStats setSpawnPoo={setSpawnPoo} />
       <DepleteStats />
       <Stats toggleStats={toggleStats} />
       {toggleAnimations && <AnimationList toggleAnimation={toggleAnimation} />}
       <div className={styles.canvas}>
-        <Experience animation={animation} />
+        <Experience animation={animation} spawnPoo={spawnPoo} />
       </div>
       <Nav
         toggleStatsClass={toggleStatsClass}
