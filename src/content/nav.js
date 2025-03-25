0fefc66e993c19be2d0a3db0cf2f70983343b2df
Ryan <ryan.irani@gmail.com>
2025-03-25 13:44:26 +0000

    update property names

--- src/content/nav.js
+++ src/content/nav.js
@@ -259,15 +259,15 @@ export const HeaderItems = [
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
         color="#cdab8f"
-        stroke-width="1.5"
+        strokeWidth="1.5"
       >
         <path
           d="M6.90588 4.53682C6.50592 4.2998 6 4.58808 6 5.05299V18.947C6 19.4119 6.50592 19.7002 6.90588 19.4632L18.629 12.5162C19.0211 12.2838 19.0211 11.7162 18.629 11.4838L6.90588 4.53682Z"
           fill="#cdab8f"
           stroke="#cdab8f"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         ></path>
       </svg>
       // <svg
