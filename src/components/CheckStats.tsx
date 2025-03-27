8ffd183532f668b93b7ef886a244525501eae78e
Ryan <ryan.irani@gmail.com>
2025-03-27 13:47:46 +0000

    fix bug resulting in multiple poos

--- src/components/CheckStats.tsx
+++ src/components/CheckStats.tsx
@@ -1,5 +1,5 @@
 import { useStats } from "@/providers/StatsProvider";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 interface CheckStatsProp {
   setSpawnPoo: (state: boolean) => void;
@@ -7,15 +7,25 @@ interface CheckStatsProp {
 export default function CheckStats({ setSpawnPoo }: CheckStatsProp) {
   const { stats } = useStats();
   const hygieneValue = Math.ceil(stats.hygiene.value);
+  const [hasSpawnedPoo, setHasSpawnedPoo] = useState(false);
 
   useEffect(() => {
     if (hygieneValue === 100) {
       setSpawnPoo(false);
+      setHasSpawnedPoo(false);
       return;
     }
 
-    setSpawnPoo(hygieneValue % 25 === 0);
-  }, [hygieneValue, setSpawnPoo]);
+    if (hygieneValue % 25 === 0 && hygieneValue !== 0 && !hasSpawnedPoo) {
+      setSpawnPoo(true);
+      setHasSpawnedPoo(true);
+      return;
+    }
+
+    if (hygieneValue > 0 && hygieneValue % 25 !== 0 && hasSpawnedPoo) {
+      setHasSpawnedPoo(false);
+    }
+  }, [hygieneValue, setSpawnPoo, hasSpawnedPoo]);
 
   return null;
 }
