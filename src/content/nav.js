0ebae3526d22c4ed0946640427ed0edd39eb9424
Ryan <ryan.irani@gmail.com>
2025-03-30 19:34:59 +0100

    update invalid properties

--- src/content/nav.js
+++ src/content/nav.js
@@ -88,7 +88,7 @@ export const NavItems = [
       <svg
         width="40px"
         height="40px"
-        stroke-width="1.5"
+        strokeWidth="1.5"
         viewBox="0 0 24 24"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
@@ -97,23 +97,23 @@ export const NavItems = [
         <path
           d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
           stroke="#e5a50a"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         ></path>
         <path
           d="M18.5716 4.46234C15.905 8.99175 15.9049 14.1847 18.5716 19.5377"
           stroke="#e5a50a"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         ></path>
         <path
           d="M5.42834 4.46234C8.09501 8.99175 8.09513 14.1847 5.42834 19.5377"
           stroke="#e5a50a"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         ></path>
       </svg>
       // <svg
@@ -196,7 +196,7 @@ export const NavItems = [
         width="40px"
         height="40px"
         viewBox="0 0 24 24"
-        stroke-width="1.5"
+        strokeWidth="1.5"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
         color="#"
@@ -204,23 +204,23 @@ export const NavItems = [
         <path
           d="M21 4V20C21 21.1046 20.1046 22 19 22H5C3.89543 22 3 21.1046 3 20V4C3 2.89543 3.89543 2 5 2H19C20.1046 2 21 2.89543 21 4Z"
           stroke="#FF6347"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         ></path>
         <path
           d="M3 8L11 8L11 6"
           stroke="#FF6347"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         ></path>
         <path
           d="M21 8L13 8L13 6"
           stroke="#FF6347"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         ></path>
       </svg>
     ),
