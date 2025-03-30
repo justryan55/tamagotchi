99bd6bb1d3e9f7f718d5026216447f04e0c5896e
Ryan <ryan.irani@gmail.com>
2025-03-30 19:34:23 +0100

    add UseEffect to component

--- src/components/WeatherData.tsx
+++ src/components/WeatherData.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import weatherIcons from "@/content/weatherIcons";
 import Image from "next/image";
 
@@ -8,43 +8,45 @@ export default function WeatherData() {
 
   const apiKey = process.env.WEATHER_API_KEY;
 
-  if (!navigator.geolocation) return;
+  useEffect(() => {
+    if (!navigator.geolocation) return;
 
-  navigator.geolocation.getCurrentPosition(
-    (position) => {
-      const { latitude, longitude } = position.coords;
-      fetchWeather(latitude, longitude);
-    },
-    () => {
-      console.log("Unable to fetch your position");
-    }
-  );
-
-  const fetchWeather = async (lat: number, lon: number) => {
-    try {
-      const res = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
-      );
-      const data = await res.json();
-      const weather = data.weather[0];
-
-      if (weather.main === "Clouds") {
-        setWeather(weatherIcons.clouds);
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        fetchWeather(latitude, longitude);
+      },
+      () => {
+        console.log("Unable to fetch your position");
       }
+    );
 
-      if (weather.main === "Clear") {
-        setWeather(weatherIcons.clear);
+    const fetchWeather = async (lat: number, lon: number) => {
+      try {
+        const res = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
+        );
+        const data = await res.json();
+        const weather = data.weather[0];
+
+        if (weather.main === "Clouds") {
+          setWeather(weatherIcons.clouds);
+        }
+
+        if (weather.main === "Clear") {
+          setWeather(weatherIcons.clear);
+        }
+
+        if (weather.main === "Rain") {
+          setWeather(weatherIcons.rain);
+        }
+
+        setIsLoading(false);
+      } catch {
+        console.log("Unable to fetch the weather");
       }
-
-      if (weather.main === "Rain") {
-        setWeather(weatherIcons.rain);
-      }
-
-      setIsLoading(false);
-    } catch {
-      console.log("Unable to fetch the weather");
-    }
-  };
+    };
+  }, []);
 
   return (
     <div>
